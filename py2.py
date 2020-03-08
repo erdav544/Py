5eb5028efae5723f366b1ce616eca541d16a1b1e
ERDAV <50980178+erdav544@users.noreply.github.com>
2020-03-08 18:13:36 +0700

    Update py2.py

---
+++ py2.py
@@ -0,0 +1,23 @@
+# CREATED BY HENDRICK 
+# 06 - 03 - 2020
+
+print (' <--- START PROGRAM --> ')
+
+print ('\n') # Buat line kosong
+
+x = int(input('Masukan bilangan x : ')) # Mengambil bilangan dan dimasukan ke variable
+y = int(input('Masukan bilangan y : ')) # Mengambil bilangan dan dimasukan ke variable
+
+print ('\n') #Buat line kosong
+
+print ('Nilai awal x : %d' % x) # Menunjukan nilai awal
+print ('Nilai awal y : %d' % y) # Menunjukan nilai awal
+
+print ('\n') # Buat line kosong
+
+print ('Nilai x : %d' % y) # Membalikan nilai
+print ('Nilai y : %d' % x) # membalikan nilai
+
+print ('\n') # Buat line kosong
+
+print (' <--- /END PROGRAM ---> ')

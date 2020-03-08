837323ea0083bb41a42741f2f4f83bfc84ee8941
ERDAV <50980178+erdav544@users.noreply.github.com>
2020-03-08 18:24:29 +0700

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,12 +1,11 @@
 #CREATED BY HENDRICK
 #06 - 03 - 20
-#CLEAN CODE STYLE
 
 print (' <!-- START PROGRAM --> ')
 
 print ('\n') # Create empty line
 
-for first in range(2, 12, 2): # Create the sequences
+for first in range(2, 12, 2): # Create sequence
 
     print(first, end=' ') # Print the program
 
